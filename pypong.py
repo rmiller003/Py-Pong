6c96cac20d4deb9705559de4850a480ce538c9b7
rmiller003 <54287828+rmiller003@users.noreply.github.com>
2021-05-04 19:33:18 -0400

    stage 2

--- pypong.py
+++ pypong.py
@@ -16,7 +16,7 @@ game_font = pygame.font.SysFont('Ubuntu', 40)
 
 delay = 30
 
-paddel_speed = 20
+paddle_speed = 20
 
 paddle_width = 10
 paddle_height = 100
@@ -41,6 +41,59 @@ ball_width = 0
 ball_x_vel = -10
 ball_y_vel = 0
 
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+
+# drawing objects
+def draw_object():
+    pygame.draw.rect(screen, WHITE, (int(p1_x_pos), int(p1_y_pos), paddle_width, paddle_height))
+    pygame.draw.rect(screen, WHITE, (int(p2_x_pos), int(p2_y_pos), paddle_width, paddle_height))
+    pygame.draw.circle(screen, WHITE, (ball_x_pos, ball_y_pos), ball_width)
+    score = game_font.render(f"{str(p1_score)} - {str(p2_score)}", False, WHITE)
+    screen.blit(score, (WIDTH / 2, 30))
+
+def apply_player_movement():
+    global p1_y_pos
+    global p2_y_pos
+
+    if p1_up:
+        p1_y_pos = max(p1_y_pos - paddle_speed, 0)
+    elif p1_down:
+        p1_y_pos = min(p1_y_pos + paddle_speed, HEIGHT)
+
+        if p2_up:
+            p1_y_pos = max(p2_y_pos - paddle_speed, 0)
+        elif p2_down:
+            p1_y_pos = min(p2_y_pos + paddle_speed, HEIGHT)
 
 
+def apply_ball_movement():
+    global ball_x_pos
+    global ball_y_pos
+    global ball_x_vel
+    global ball_y_vel
+    global p1_score
+    global p2_score
 
+    if (ball_x_pos + ball_x_vel < p1_x_pos + paddle_width) and (p1_y_pos < ball_y_pos + ball_y_vel + ball_width < p1_y_pos + paddle_height):
+        ball_x_vel = -ball_x_vel
+        ball_y_vel = (p1_y_pos + paddle_height / 2 - ball_y_pos) / 15
+        ball_y_vel = -ball_y_vel
+    elif ball_x_pos + ball_x_vel < 0:
+        p2_score += 1
+        ball_x_pos = WIDTH / 2
+        ball_x_pos = HEIGHT / 2
+        ball_x_vel = 10
+        ball_y_vel = 0
+    if (ball_x_pos + ball_x_vel > p2_x_pos - paddle_width) and (p2_y_pos < ball_y_pos + ball_y_vel + ball_width < p2_y_pos + paddle_height):
+        ball_x_vel = -ball_x_vel
+        ball_y_vel = (p2_y_pos + paddle_height / 2 - ball_y_pos) / 15
+        ball_y_vel = -ball_y_vel
+    elif ball_x_pos + ball_x_vel > HEIGHT:
+        p2_score += 1
+        ball_x_pos = WIDTH / 2
+        ball_x_pos = HEIGHT / 2
+        ball_x_vel = -10
+        ball_y_vel = 0
+
+    if ball_y_pos + ball_x_vel > HEIGHT or ball_y_pos + ball_y_vel < 0:
+        ball_y_vel = -ball_y_vel
